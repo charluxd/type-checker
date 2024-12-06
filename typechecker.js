302c094f39936c88e814a02302d9123438db5353
Charlotte Lee <charlotteuxd@gmail.com>
2024-12-06 18:34:03 +0800

    Added settings

--- typechecker.js
+++ typechecker.js
@@ -1,5 +1,13 @@
 const sentenceTag = document.querySelector(`input[type = 'text']`);
-const typesizeTag = document.querySelector(`input[name] ="typesize"]`);
+
+const typesizeTag = document.querySelector(`input[name = 'typesize']`);
+const typesizeOutput = document.querySelector(`span.typesize-output`);
+
+const lineheightTag = document.querySelector(`input[name = 'leading']`);
+const lineheightOutput = document.querySelector(`span.lineheight-output`);
+
+const italicTag = document.querySelector(`input[name = 'italic']`);
+
 const outputTag = document.querySelector('textarea.output');
 const originalText = outputTag.value;
 
@@ -20,3 +28,24 @@ outputTag.addEventListener('keyup', function () {
 
 //when I change my typesize slider, update the text next to it AND,
 //change the outputTag's font size
+typesizeTag.addEventListener('input', function () {
+  outputTag.style.fontSize = this.value + 'px';
+  typesizeOutput.innerHTML = this.value + 'px';
+});
+
+//when I change my leading slider, update the text next to it AND,
+//change the outputTag's leading
+lineheightTag.addEventListener('input', function () {
+  outputTag.style.lineHeight = this.value;
+  lineheightOutput.innerHTML = this.value;
+});
+
+//when I change my italic checkbox, update the font style to either
+//normal or italic depending if it's checked or unchecked
+italicTag.addEventListener('change', function () {
+  if (this.checked) {
+    outputTag.style.fontStyle = 'italic';
+  } else {
+    outputTag.style.fontStyle = 'normal';
+  }
+});
